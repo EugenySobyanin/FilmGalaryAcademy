68e3d50b5647b863faafa410b0a7240106d2188f
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-07-23 17:36:26 +0300

    Функционал.

--- TestWpfApp/MainViewModel.cs
+++ TestWpfApp/MainViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using FilmGalary.Core.Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestWpfApp.Core;
+using FilmGalary.Core.Entity;
+using FilmGalary.Core.Service;
 
 namespace TestWpfApp
 {
     class MainViewModel: ObservableObject
     {
-        //public FilmGalaryDataSource _dataSource = new FilmGalaryDataSource();
-        //public FilmService _service = new FilmService(_dataSource);
+        private ObservableCollection<Film> _filmlist = new ObservableCollection<Film>();
+        public ObservableCollection<Film> Filmlist { get => _filmlist; set {_filmlist = value; OnPropertyChanged("FilmList");}}
+        private FilmService _filmservice;
+
 
     }
 }
